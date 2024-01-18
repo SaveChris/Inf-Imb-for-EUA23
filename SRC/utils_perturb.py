1a4f9acc664f75e0167c8420f8db2dc550f884d6
CRISTIANO SALVAGNIN <c.salvagnin@unibs.it>
2024-01-18 15:26:07 +0100

    Update utils_perturb.py

--- SRC/utils_perturb.py
+++ SRC/utils_perturb.py
@@ -15,7 +15,6 @@ def return_inf_imb_perturbed_d1_to_d2(d1: Data, d2: Data, k=1, n_perturbations=1
     Returns:
         mean and standard deviation of the imbalance estimates from d1 to d2.
     """
-
     imbalances_X1toX2 = []
     for i in range(n_perturbations):
         if sigma is None:
@@ -24,18 +23,13 @@ def return_inf_imb_perturbed_d1_to_d2(d1: Data, d2: Data, k=1, n_perturbations=1
         else:
             X1_i = d1.X + np.random.normal(0, sigma, d1.X.shape)
             X2_i = d2.X + np.random.normal(0, sigma, d2.X.shape)
-
         _, d1_dist_indices = compute_nn_distances(
             X1_i, d1.maxk, d1.metric, d1.period
         )  
         _, d2_dist_indices = compute_nn_distances(
             X2_i, d2.maxk, d2.metric, d2.period
         )
-
         imb_X1toX2 = _return_imbalance(d1_dist_indices, d2_dist_indices, k=k)
-
         imbalances_X1toX2.append(imb_X1toX2)
-    
     mean, std = np.mean(imbalances_X1toX2), np.std(imbalances_X1toX2)
-
     return mean, std
